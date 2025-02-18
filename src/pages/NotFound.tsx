df683d02b3e751ad475fd7d57712ec7339f0d7dc
wjh1010 <42775656+wjh1010@users.noreply.github.com>
2025-02-19 07:21:43 +0900

    🎨 Style: 404페이지 퍼블리싱

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className="flex flex-col gap-10 items-center justify-center h-[calc(100vh-84px)] text-center">
+      <h2 className="text-6xl font-bold text-gray-800">Page Not Found</h2>
+      <p className="text-body-n-r text-gray-80 mt-2">
+        요청하신 페이지를 찾을 수 없어요.
+      </p>
+      <Link
+        to="/"
+        className="px-6 py-4 text-lg text-gray-80 bg-blue-7 rounded-[8px]  hover:text-base-1 hover:bg-blue-4 transition"
+      >
+        홈으로 돌아가기
+      </Link>
     </div>
   );
 };
