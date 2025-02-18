96e0f5320f3c18882f7b4609d2deb529738a3247
wjh1010 <42775656+wjh1010@users.noreply.github.com>
2025-02-18 14:51:35 +0900

    🎨 Style: 로그인 페이지 퍼블리싱

---
+++ src/components/common/InputField.tsx
@@ -0,0 +1,36 @@
+interface InputFieldProps {
+  label: string;
+  name: string;
+  placeholder: string;
+  errorMessage?: string;
+}
+
+const InputField = ({
+  label,
+  name,
+  placeholder,
+  errorMessage,
+}: InputFieldProps) => {
+  return (
+    <div className="mb-4 w-[500px]">
+      <label
+        htmlFor={name}
+        className="block mt-6 pl-2 text-blue-1 body-l-r mb-2"
+      >
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        type="text"
+        placeholder={placeholder}
+        className="w-[500px] h-[50px] px-4 border border-blue-7 rounded-[8px] focus:outline-none focus:ring-1 focus:ring-blue-1 text-body-s-r placeholder-gray-30"
+      />
+      {errorMessage && (
+        <p className="text-red-500 text-sm mt-1 pl-2">{errorMessage}</p>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,63 +1,59 @@
+import GoogleLogo from "../../assets/GoogleLogo.svg";
+import KakaoLogo from "../../assets/KakaoLogo.svg";
+import InputField from "../../components/common/InputField";
+import CustomButton from "../../components/ui/CustomButton";
+
 const Login = () => {
   return (
     <div className="max-w-[1200px] mx-10 w-full flex flex-col items-center justify-center h-screen">
-      <h2 className="text-4xl font-serif mt-[264px] mb-[250px]">On culture</h2>
+      <h2 className="text-4xl mt-[264px] mb-[250px] font-serif italic text-blue-1">
+        On culture
+      </h2>
 
       <div className="w-full max-w-md p-8 rounded-[8px] flex flex-col items-center ">
-        {/* 이메일 입력 */}
-        <div className="mb-4 w-[500px]">
-          <label className="block text-gray-700 pl-2 text-sm font-bold mb-2">
-            이메일
-          </label>
-          <input
-            type="email"
-            placeholder="이메일을 입력해주세요."
-            className="w-[500px] h-[50px] px-4 border  border-gray-300 rounded-[8px] focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-          <p className="text-red-500 text-sm mt-1 pl-2">
-            이메일을 확인해주세요.
-          </p>
-        </div>
+        <InputField
+          label="이메일"
+          name="email"
+          placeholder="이메일을 입력해주세요."
+          errorMessage="이메일을 확인해주세요."
+        />
+        <InputField
+          label="비밀번호"
+          name="password"
+          placeholder="비밀번호를 입력해주세요."
+          errorMessage="비밀번호를 확인해주세요."
+        />
 
-        {/* 비밀번호 입력 */}
-        <div className="mb-4 relative w-[500px]">
-          <label className="block mt-6 pl-2 text-gray-700 text-sm font-bold mb-2">
-            비밀번호
-          </label>
-          <input
-            type="password"
-            placeholder="비밀번호를 입력해주세요."
-            className="w-[500px] h-[50px] px-4 border border-gray-300 rounded-[8px] focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-          <p className="text-red-500 text-sm mt-1 pl-2">
-            비밀번호를 입력해주세요.
-          </p>
-        </div>
-
-        {/* 버튼들 */}
-        <button className="w-[500px] h-[50px] bg-blue-300 text-white rounded-[8px] hover:bg-blue-400 transition mt-10 mb-7">
-          로그인
-        </button>
-        <button className="w-[500px] h-[50px] bg-blue-500 text-white rounded-[8px] hover:bg-blue-600 transition ">
-          이메일 회원가입
-        </button>
-
+        <CustomButton
+          text="로그인"
+          bgColor="bg-blue-7"
+          onClick={() => console.log("로그인 클릭")}
+        />
+        <CustomButton
+          text="이메일 회원가입"
+          bgColor="bg-blue-7"
+          onClick={() => console.log("이메일 회원가입 클릭")}
+        />
         {/* 구분선 */}
-        <div className="my-8 flex items-center w-[500px]">
+        <div className="mb-8 flex items-center w-[500px]">
           <hr className="flex-grow border-gray-300" />
           <span className="mx-4 text-gray-500 text-sm">간편 로그인</span>
           <hr className="flex-grow border-gray-300" />
         </div>
-
-        {/* Google 로그인 */}
-        <button className="w-[500px] h-[50px] flex items-center justify-center bg-white border rounded-[8px] shadow hover:shadow-md transition mb-6">
-          Google로 시작하기
-        </button>
-
-        {/* Kakao 로그인 */}
-        <button className="w-[500px] h-[50px] flex items-center justify-center bg-yellow-300 rounded-[8px] hover:bg-yellow-400 transition">
-          Kakao로 시작하기
-        </button>
+        <CustomButton
+          text="Google로 시작하기"
+          bgColor="bg-white"
+          borderColor="border-blue-1"
+          iconSrc={GoogleLogo}
+          onClick={() => console.log("구글 로그인 클릭")}
+        />
+        <CustomButton
+          text="Kakao로 시작하기"
+          bgColor="bg-white"
+          borderColor="border-blue-1"
+          iconSrc={KakaoLogo}
+          onClick={() => console.log("카카오 로그인 클릭")}
+        />
       </div>
     </div>
   );
