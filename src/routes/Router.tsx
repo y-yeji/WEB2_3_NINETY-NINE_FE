42ab7bbdcaa324d54bded2b56ffe306ff457eafd
dilma01 <dilma01@naver.com>
2025-02-20 11:51:10 +0900

    ✨ Feat: 정보 상세 페이지 추가 & 라우팅

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -12,6 +12,7 @@ import Musical from "../pages/informations/Musical";
 import PopupStore from "../pages/informations/PopupStore";
 import MapSearch from "../pages/mapsearch/MapSearch";
 import RootLayout from "../layouts/RootLayout";
+import InfoCardDetail from "../pages/informations/InfoCardDetail";
 
 const Router = () => {
   return (
@@ -28,6 +29,7 @@ const Router = () => {
         <Route path="/musical" element={<Musical />} />
         <Route path="/popupstore" element={<PopupStore />} />
         <Route path="/mapsearch" element={<MapSearch />} />
+        <Route path="/infocarddetail" element={<InfoCardDetail />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
