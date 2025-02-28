a5f0d7d8f8f75fb730fc03741428c3b1c55508f3
wjh1010 <42775656+wjh1010@users.noreply.github.com>
2025-02-28 16:44:19 +0900

    ✨ Feat: 메인 페이지 기능 구현

---
+++ src/api/eventApi.ts
@@ -0,0 +1,11 @@
+import api from "../api/api";
+
+export const fetchEventData = async (eventType: string) => {
+  try {
+    const response = await api.get(`/api/events/${eventType}`);
+    return response.data;
+  } catch (error) {
+    console.error(`${eventType} 데이터를 불러오는 중 오류 발생:`, error);
+    return [];
+  }
+};
