489403a333b217f07241c49dc61148c2bfc678c4
dilma01 <dilma01@naver.com>
2025-03-06 05:35:34 +0900

    ✨ Feat: 카카오톡 공유하기 기능 구현 (API 연결X)

---
+++ src/types/Informationdetail.ts
@@ -0,0 +1,12 @@
+export interface ShowInfo {
+  posterUrl: string;
+  title: string;
+  audience: string;
+  period: string;
+  location: string;
+  times: string;
+  links: string;
+  price: string;
+  reviewCount: number;
+  isBookmarked: boolean;
+}
