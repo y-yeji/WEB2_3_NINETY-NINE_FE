0f01dc2f1c5b374ad71a36e759528e93c354c4da
dilma01 <dilma01@naver.com>
2025-03-06 05:36:46 +0900

    ✨ Feat: 정보 상세 페이지의 컴포넌트 분리

--- src/pages/informations/InfoCardDetail.tsx
+++ src/pages/informations/InfoCardDetail.tsx
@@ -2,19 +2,8 @@ import { useEffect, useState } from "react";
 import Icon from "../../assets/icons/Icon";
 import InfoCardDetailInfo from "./InfoCardDetailInfo";
 import InfoCardDetailReview from "./InfoCardDetailReview";
-
-interface ShowInfo {
-  posterUrl: string;
-  title: string;
-  audience: string;
-  period: string;
-  location: string;
-  times: string;
-  links: string;
-  price: string;
-  reviewCount: number;
-  isBookmarked: boolean;
-}
+import InfoCardHeader from "../../components/informationdetail/InfoCardHeader"; // Import the new component
+import { ShowInfo } from "../../types/Informationdetail";
 
 const dummyShowInfo: ShowInfo = {
   posterUrl: "/poster-img.png",
@@ -57,75 +46,11 @@ const InfoCardDetail = () => {
 
   return (
     <div className="w-[1280px] h-full relative overflow-hidden bg-white">
-      <div className="w-[1120px] h-[470px] mx-auto mt-[130px] rounded-[10px] bg-[#f9f9f9] border border-blue-7 p-9 flex relative">
-        <img
-          src={dummyShowInfo.posterUrl}
-          alt="전시 포스터"
-          className="w-[300px] h-[400px] ml-20"
-        />
-        <div className="flex flex-col justify-between ml-32">
-          <p className="h3-b font-bold text-left text-gray-80 mb-24 mt-3">
-            {dummyShowInfo.title}
-          </p>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-1.5">
-              <Icon name="UsersRound" className="w-[18px] h-[18px] ml-0.5" />
-              <p className="body-normal-r text-left text-gray-80">
-                {dummyShowInfo.audience}
-              </p>
-            </div>
-            <div className="flex items-center gap-1.5">
-              <Icon name="CalendarRange" className="w-5 h-5" />
-              <p className="body-normal-r text-left text-gray-80">
-                {dummyShowInfo.period}
-              </p>
-            </div>
-            <div className="flex items-center gap-1.5">
-              <Icon name="MapPin" className="w-5 h-5" />
-              <p className="body-normal-r text-left text-gray-80">
-                {dummyShowInfo.location}
-              </p>
-            </div>
-            <div className="flex items-center gap-1.5">
-              <Icon name="Clock" className="w-[18px] h-[18px] ml-0.5" />
-              <p className="body-normal-r text-left text-gray-80">
-                {dummyShowInfo.times}
-              </p>
-            </div>
-            <div className="flex items-center gap-[5px]">
-              <Icon
-                name="Pin"
-                className="w-[18px] h-[18px] ml-0.5"
-                style={{ transform: "rotate(-22.23deg)" }}
-              />
-              <p className="body-normal-r text-left text-gray-80">
-                {dummyShowInfo.links}
-              </p>
-            </div>
-            <div className="flex items-center gap-1.5">
-              <Icon name="Tickets" className="w-[18px] h-[18px] ml-0.5" />
-              <p className="body-normal-r text-left text-gray-80">
-                {dummyShowInfo.price}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-4 mt-2 absolute bottom-7 right-7">
-          <button
-            className="w-8 h-8 rounded-[8px] bg-blue-6 hover:bg-blue-6/80 flex justify-center items-center"
-            onClick={toggleBookmark}
-          >
-            <Icon
-              name="Bookmark"
-              className="w-6 h-6 text-white"
-              fill={isBookmarked ? "white" : "none"}
-            />
-          </button>
-          <button className="w-8 h-8 rounded-[8px] bg-blue-6 hover:bg-blue-6/80 flex justify-center items-center">
-            <Icon name="Share2" className="w-6 h-6 text-white" />
-          </button>
-        </div>
-      </div>
+      <InfoCardHeader
+        showInfo={dummyShowInfo}
+        isBookmarked={isBookmarked}
+        toggleBookmark={toggleBookmark}
+      />
 
       <div className="flex flex-col items-center mt-12">
         <div className="w-[1120px] flex justify-center relative">
